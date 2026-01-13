d56755921219f2e6c7165868b9d764616c975d6e
n n <36607891+PpekKOX@users.noreply.github.com>
2026-01-13 15:26:10 +0100

    Add Character model to the database

---
+++ models.py
@@ -0,0 +1,11 @@
+from database import db
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    world = db.Column(db.String(32), nullable=False)
+    level = db.Column(db.Integer)
+    map = db.Column(db.String(64))
+    x = db.Column(db.Integer)
+    y = db.Column(db.Integer)
+    last_online = db.Column(db.DateTime)
