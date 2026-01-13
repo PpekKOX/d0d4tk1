22c19c3b7d9a7a9f9691d48ee9e9e8b7a00a3b02
n n <36607891+PpekKOX@users.noreply.github.com>
2026-01-13 15:29:24 +0100

    Add level, map, coordinates, and last_online fields

--- models.py
+++ models.py
@@ -4,8 +4,13 @@ class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     world = db.Column(db.String(32), nullable=False)
+
     level = db.Column(db.Integer)
     map = db.Column(db.String(64))
     x = db.Column(db.Integer)
     y = db.Column(db.Integer)
     last_online = db.Column(db.DateTime)
+
+    __table_args__ = (
+        db.UniqueConstraint("name", "world", name="uniq_character_world"),
+    )
