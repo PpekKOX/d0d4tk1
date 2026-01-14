faaf96917c05afa34d968580c939f0260319c8e4
n n <36607891+PpekKOX@users.noreply.github.com>
2026-01-14 18:29:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ from datetime import datetime, timedelta
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db_url = os.getenv("DATABASE_URL", "sqlite:///data.db")
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
@@ -158,6 +162,7 @@ def get_online_world(world):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
 
